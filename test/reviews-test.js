3691d2c441e86be2f1cd448d7afe2700938a9b71
Srinivasan Raguraman <ThoughtWorks@blankslate-sr5977.local>
2011-12-16 19:28:30 -0600

    using apricot - not much luck here.

--- test/reviews-test.js
+++ test/reviews-test.js
@@ -1,9 +1,21 @@
 var market = require('../lib/reviews.js');
 
-market.get_last_review('com.facebook.katana', function(chunk) {
-   var content = chunk.replace(")]}'", "");
-   content = content.replace(/[\n\r\t]/g,'');
-   
-   var jsObject = JSON.parse(content);
-   console.log(jsObject.htmlContent);
+// market.get_latest_reviews('com.facebook.katana', function(chunk) {
+//    var content = chunk.replace(")]}'", "");
+//    content = content.replace(/[\n\r\t]/g,'');
+//    
+//    var jsObject = JSON.parse(content);
+//    console.log(jsObject.htmlContent);
+// });
+
+
+market.get_last_comment('com.facebook.katana', function(comment) {
+    console.log(comment);
 });
+
+
+
+  // 
+  // apricot.parse(content, function(err, doc) {
+  //       // last_review = doc.top('doc-review').top('review-text').toHTML;
+  //   });
