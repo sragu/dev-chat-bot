06e04968025956ad59b8729f1dd1aa24b1ff28e3
Srinivasan Raguraman <ThoughtWorks@blankslate-sr5977.local>
2011-12-16 18:47:19 -0600

    able to parse the data back from google api call

--- test/reviews-test.js
+++ test/reviews-test.js
@@ -1,5 +1,9 @@
 var market = require('../lib/reviews.js');
 
 market.get_last_review('com.facebook.katana', function(chunk) {
-   console.log(chunk);
+   var content = chunk.replace(")]}'", "");
+   content = content.replace(/[\n\r\t]/g,'');
+   
+   var jsObject = JSON.parse(content);
+   console.log(jsObject.htmlContent);
 });
